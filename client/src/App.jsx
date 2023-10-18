ac3ec8e61a9c216febbc70b7dceba56e9383bb3b
lanzmoncis <lanzmoncis@gmail.com>
2023-10-18 20:23:18 +0100

    Create react app and connect to the database

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-3xl underline">Hello MERN</div>;
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDb.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...`);
+});

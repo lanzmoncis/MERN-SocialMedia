d76ea7b5cae290c56ed58c42f94e4acb2491abce
lanzmoncis <lanzmoncis@gmail.com>
2023-10-19 15:43:40 +0100

    Create post controllers and routes for createPost, getPost, deletePost, likeAndUnlikePost, replyToPost, getFeedPosts, getUserPosts

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./db/connectDb.js";
 
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
